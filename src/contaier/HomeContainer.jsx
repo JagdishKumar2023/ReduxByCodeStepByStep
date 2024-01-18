3fe347772039ccc2ccc251b53b6f717341eeb88e
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-01-18 23:17:44 +0530

    New Chanegs

--- src/contaier/HomeContainer.jsx
+++ src/contaier/HomeContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomeContainer = () => {
   return (
-    <div>
+    <div className="mainContainer">
       <h1>Home Components</h1>
       <div className="cart-wrapper">
         <div className="img-wrapper item">
@@ -10,16 +10,18 @@ const HomeContainer = () => {
             src="https://i.pinimg.com/736x/5b/95/b0/5b95b05d61d9d6c2e055f33b31c1d16d.jpg"
             alt="iphone"
           />
-          <img
-            src="https://freepngimg.com/save/25486-add-to-cart-button/763x220"
-            alt="Add To Cart"
-          />
         </div>
         <div className="text-wrapper item">
           <span>I- phone 15 Ultra</span>
           <span>Price: 1000$.00</span>
           <button>Add To cart</button>
         </div>
+      </div>
+      <div className="addCartImgDiv">
+        <img
+          src="https://png.pngtree.com/png-vector/20190501/ourmid/pngtree-add-to-cart--icon-design-png-image_1013293.jpg"
+          alt="Add to cart"
+        />
       </div>
     </div>
   );
